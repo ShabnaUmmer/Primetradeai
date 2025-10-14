fa29f03486b3fcd2099aa2bc11b9088f1ef96886
Shabna Ummer <147902234+ShabnaUmmer@users.noreply.github.com>
2025-10-15 01:00:41 +0530

    app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -70,11 +70,7 @@ app.listen(PORT, () => {
 
 module.exports = app;
 
-if (process.env.NODE_ENV !== 'production') {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
+
+
 
 
